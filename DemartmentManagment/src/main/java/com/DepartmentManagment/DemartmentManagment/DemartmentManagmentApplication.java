6feae86d8af574235cf1851249cdb93748793552
AKASHBARDE <akashvsbarde@gmail.com>
2023-10-26 13:43:34 +0530

    new

--- DemartmentManagment/src/main/java/com/DepartmentManagment/DemartmentManagment/DemartmentManagmentApplication.java
+++ DemartmentManagment/src/main/java/com/DepartmentManagment/DemartmentManagment/DemartmentManagmentApplication.java
@@ -9,5 +9,6 @@ public class DemartmentManagmentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemartmentManagmentApplication.class, args);
 	}
-	//google
+	//gradle
+
 }
